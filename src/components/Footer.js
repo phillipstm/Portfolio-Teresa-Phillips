c0e90a1e8daf4b477ec017733cc87cfc0c0488f8
Teresa <luvmargaritas@gmail.com>
2022-11-28 13:10:46 -0600

    made some updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -31,7 +31,7 @@ class Footer extends Component {
               >
                 <span
                   className="iconify language-icon mr-5"
-                  data-icon="twemoji-flag-for-flag-united-kingdom"
+                  data-icon="twemoji-flag-for-flag-united-states"
                   data-inline="false"
                   id={window.$primaryLanguageIconId}
                 ></span>
